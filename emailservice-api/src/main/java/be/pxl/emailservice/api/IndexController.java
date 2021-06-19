bd52ab0f1aae3dae48f77b2734735596ee907fc6
elindebu <eljakim.lindeburg@vdab.be>
2021-06-19 22:31:19 +0200

    up and running

--- emailservice-api/src/main/java/be/pxl/emailservice/api/IndexController.java
+++ emailservice-api/src/main/java/be/pxl/emailservice/api/IndexController.java
@@ -1,12 +1,14 @@
 package be.pxl.emailservice.api;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/")
 public class IndexController {
 
-    @GetMapping("/")
+    @GetMapping
     public String index() {
         return "Emailservice is up and running voor het juryexamen.";
     }
