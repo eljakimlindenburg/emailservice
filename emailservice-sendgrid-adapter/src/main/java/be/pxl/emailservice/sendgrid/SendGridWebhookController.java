7a0b7070e2a25917ff7becb7ee4af15bc5681171
elindebu <eljakim.lindeburg@vdab.be>
2021-06-20 19:44:41 +0200

    webhook event improvement

--- emailservice-sendgrid-adapter/src/main/java/be/pxl/emailservice/sendgrid/SendGridWebhookController.java
+++ emailservice-sendgrid-adapter/src/main/java/be/pxl/emailservice/sendgrid/SendGridWebhookController.java
@@ -1,7 +1,10 @@
 package be.pxl.emailservice.sendgrid;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 import be.pxl.emailservice.core.api.service.EmailService;
 import be.pxl.emailservice.sendgrid.events.BounceDto;
+import org.slf4j.Logger;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/api/sendgrid")
 public class SendGridWebhookController {
 
+    private static final Logger LOGGER = getLogger(SendGridWebhookController.class);
+
     private final EmailService emailService;
 
     public SendGridWebhookController(EmailService emailService) {
@@ -19,6 +24,7 @@ public class SendGridWebhookController {
 
     @PostMapping(path = "/bounce")
     public void processBounce(@RequestBody BounceDto dto) {
+        LOGGER.info("webhook event ontvangen: {}", dto);
         if (dto.getEvent().equalsIgnoreCase("bounce")) {
             emailService.verwerkBounce(dto.getEmail());
         }
